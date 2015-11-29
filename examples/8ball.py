3720cefe6752f8978010a85e9842f3e93eb7fbe1
martinohanlon <martin@ohanlonweb.com>
2015-11-29 22:56:24 +0000

    Added magic 8 ball

---
+++ examples/8ball.py
@@ -0,0 +1,30 @@
+import microbit
+
+TOLERANCE = 3000
+MESSAGES = ["It is certain", "Dont count on it", "Ask again later", "Ask again"]
+
+def get_accel_total():
+    x = microbit.accelerometer.get_x()
+    y = microbit.accelerometer.get_y()
+    z = microbit.accelerometer.get_z()
+    return x + y + z
+
+def wait_for_shake():
+    shaken = False
+    last = get_accel_total()
+    while not shaken:
+        this = get_accel_total()
+        diff = last - this
+        if diff < 0: diff = diff * -1
+        if diff > TOLERANCE:
+            shaken = True
+        last = this
+        microbit.sleep(50)
+
+while True:
+    microbit.display.print("8")
+    wait_for_shake()
+    microbit.display.clear()
+    microbit.sleep(2000)
+    message = microbit.random(len(MESSAGES))
+    microbit.display.scroll(MESSAGES[message])
