f7ce1e9585cf1021cde8fbd7aeba2afbdbbbda67
ct9001 <ct9001@yahoo.com>
2017-01-19 18:48:20 +0000

    Extended 8 ball

--- examples/8ball.py
+++ examples/8ball.py
@@ -1,7 +1,7 @@
 import microbit
 
 TOLERANCE = 3000
-MESSAGES = ["It is certain", "Dont count on it", "Ask again"]
+MESSAGES = ["It is certain", "Dont count on it", "Ask again""It is decidedly so", "Without a doubt", "Yes, definitely", "You may rely on it", "As I see it, yes", "Most likely". "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
 
 def get_accel_total():
     x = microbit.accelerometer.get_x()
